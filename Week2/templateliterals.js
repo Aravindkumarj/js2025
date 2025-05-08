77bcfb521cd65a49839241713032ade971c42956
Aravind Kumar Jagannathan <aravindkumarj@gmail.com>
2025-05-07 20:55:16 -0500

    Week2 final

---
+++ Week2/templateliterals.js
@@ -0,0 +1,20 @@
+let books = [
+    {
+        title: "Gone with the Wind",
+        author: "Margaret Mitchell",
+        yearPublished: 1936
+
+    },
+    {
+        title: "Then She Was Gone",
+        author: "Lisa Jewel",
+        yearPublished: 2017
+    },
+    {
+        title: "Where the Crawdads Sing",
+        author: "Delia Owens",
+        yearPublished: 2018
+    }
+]
+
+console.log(`The book '${books[0].title}' was written by  ${books[0].author} in  ${books[0].yearPublished} `);
